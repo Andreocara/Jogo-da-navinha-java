e5ba7fa61ba7a2f95dbe90be2b61fe3393706ade
André Luis <106080283+Andreocara@users.noreply.github.com>
2023-03-25 01:03:52 +0000

    0000

--- navinha.js
+++ navinha.js
@@ -1,8 +1,16 @@
 function teclaDw(){
   var tecla=Event.keyCode;
-  if
+  if(tecla==38){//cima
 
+  }else if(tecla==40){//baixo
+
+  }if(tecla==37){//esquerda
+
+  }else if(tecla==39){//direita
+
+  }
 }
+
 function teclaUp(){
 
 }
