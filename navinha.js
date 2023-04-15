3b9f8a4eaf0b5ebf70a00680f797d94a37235d51
André Luis <106080283+Andreocara@users.noreply.github.com>
2023-04-15 19:14:08 +0000

    0000

--- navinha.js
+++ navinha.js
@@ -1,63 +1,58 @@
-var diryJ,dirxJ,jog,velj,pjx,pjy;
-var tamTelaW,tamTelaH;
+var diryJ, dirxJ, jog, velj, pjx, pjy;
+var tamTelaW, tamTelaH;
 var jogo;
 var frames;
 
-function teclaDw(){
-  var tecla=Event.keyCode;
-  if(tecla==38){//cima
-    diryJ=-1;
-
-  }else if(tecla==40){//baixo
-    diryJ=1;
-
-  }if(tecla==37){//esquerda
-    dirxJ=-1;
-
-  }else if(tecla==39){//direita
-    dirxJ=1;
-
-  }if(tecla==32){tiro
+function teclaDw(event) {
+  var tecla = event.keyCode;
+  if (tecla == 38) { // cima
+    diryJ = -1;
+  } else if (tecla == 40) { // baixo
+    diryJ = 1;
+  }
+  else if (tecla == 37) { // esquerda
+    dirxJ = -1;
+  } else if (tecla == 39) { // direita
+    dirxJ = 1;
+  }
+  if (tecla == 32) { // espaço
+    // código para criar um tiro
   }
 }
 
-function teclaUp(){
-  var tecla=Event.keyCode;
-  if((tecla==38)||(tecla==40)){
-    diryJ=0
+function teclaUp(event) {
+  var tecla = event.keyCode;
+  if (tecla == 38 || tecla == 40 || tecla == 37 || tecla == 39) {
+    diryJ = dirxJ = 0;
   }
-  if((tecla==37)||(tecla==39)){esquerda
-    dirxJ=0;
-  }
-
 }
 
-function controlaJogador(){
-  pjy+=diryJ*velj;
-  pjx+=dirxJ*velj;
-  jog.style.top=pjy+"px";
-  jog.style.left=pjx+"px";
+function controlaJogador() {
+  pjy += diryJ * velj;
+  pjx += dirxJ * velj;
+  jog.style.top = pjy + "px";
+  jog.style.left = pjx + "px";
 }
 
-
-function gameLoop(){
-  if(jogo){
+function gameLoop() {
+  if (jogo) {
     controlaJogador();
   }
-  frames=requestAnimationFrame(gameLoop);
+  frames = requestAnimationFrame(gameLoop);
 }
 
-function inicia(){
-  jogo=false;
-  dirxJ=diryJ=0;
-  tamTelaH=window.innerHeight;
-  tamTelaW=window.innerWidth;
-  pjx=tamTelaW/2;
-  pjy=tamTelaH/2;
-  velj=5;
-  jog=document.getElementById("naveJogo");
+function inicia() {
+  jogo = true;
+  dirxJ = diryJ = 0;
+  tamTelaH = window.innerHeight;
+  tamTelaW = window.innerWidth;
+  pjx = tamTelaW / 2;
+  pjy = tamTelaH / 2;
+  velj = 5;
+  jog = document.getElementById("naveJ
   jog.style.top=pjy+"px";
   jog.style.left=pjx+"px";
+  gameLoop();
 }
 
 Window.addEventListener("load",inicia)
